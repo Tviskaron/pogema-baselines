4aab7693a4202876555f80652d8404d50ed49f72
tviskaron <tviskaron@gmail.com>
2022-09-02 12:19:35 +0300

    fixed error with grid_config deliverywq

--- pymarl/envs/__init__.py
+++ pymarl/envs/__init__.py
@@ -1,14 +1,13 @@
 from functools import partial
 from pogema import pogema_v0
 
-from utils.config_validation import ExperimentConfig
+from utils.config_validation import Environment, ExperimentConfig
 
 
 def env_fn(**kwargs):
-    cfg = ExperimentConfig(**kwargs)
-    cfg.env_args.grid_config.integration = 'PyMARL'
-    return pogema_v0(grid_config=cfg.env_args.grid_config)
-
+    cfg = Environment(**kwargs)
+    cfg.grid_config.integration = 'PyMARL'
+    return pogema_v0(grid_config=cfg.grid_config)
 
 REGISTRY = {}
 

3aeb326083591477c3f7f8c80774d16edd29056b
Alexey Skrynnik <Tviskaron@gmail.com>
2022-09-02 10:35:51 +0300

    Update __init__.py
    hotfix

--- pymarl/envs/__init__.py
+++ pymarl/envs/__init__.py
@@ -7,7 +7,7 @@ from utils.config_validation import ExperimentConfig
 def env_fn(**kwargs):
     cfg = ExperimentConfig(**kwargs)
     cfg.env_args.grid_config.integration = 'PyMARL'
-    return pogema_v0(cfg.env_args.grid_config)
+    return pogema_v0(grid_config=cfg.env_args.grid_config)
 
 
 REGISTRY = {}
